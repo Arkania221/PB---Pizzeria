353efd1a1f91179c4844465ce27109cc02effdca
Ángel Borrás <angeloki1963@gmail.com>
2024-02-01 10:01:26 +0100

    Iniciando programa

---
+++ main.py
@@ -0,0 +1,23 @@
+opcion = input("Selecciona una opcion: ")
+comanda_completa = []
+if opcion == 1:
+    while True:
+        print("1 - Hamburguesa")
+        print("2 - Pizza de glock")
+        print("3 - Limon")
+        print("4 - mas pizza")
+        print("5 - Pizza de tierra")
+        print("6 - Finalizar pedido")
+        comanda = input("Seleccione su pedido: ")
+        if comanda = 1:
+            comanda_completa.append("Hamburguesa")
+        if comanda = 2:
+            comanda_completa.append("Pizza de glock")
+        if comanda = 3:
+            comanda_completa.append("Limon")
+        if comanda = 4:
+            comanda_completa.append("mas Pizza")
+        if comanda = 5:
+            comanda_completa.append("Pizza de tierra")
+        if comanda = 6:
+            break
