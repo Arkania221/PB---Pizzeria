e0dae1e1558556a9cfe345e4177f2032ebfba9ee
Ángel Borrás <angeloki1963@gmail.com>
2024-02-02 08:29:16 +0100

    Añadido los if's del resto de opciones y arreglado problemas de seleccio debido a que se almacenaba la variable elegida como str en vez de int. ademas se intenta probar hacer un temporizador con un for in range

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-opcion = input("Selecciona una opcion: ")
+print("1 - Seleccionar pedido / 2 - Visualizar preparacion / 3 - Gestionar Stock / 4 - Procesar Pago")
+opcion = int(input("Selecciona una opcion: "))
 comanda_completa = []
+
 if opcion == 1:
     while True:
         print("1 - Hamburguesa")
@@ -8,16 +10,37 @@ if opcion == 1:
         print("4 - mas pizza")
         print("5 - Pizza de tierra")
         print("6 - Finalizar pedido")
-        comanda = input("Seleccione su pedido: ")
-        if comanda = 1:
+        comanda = int(input("Seleccione su pedido: "))
+        if comanda == 1:
             comanda_completa.append("Hamburguesa")
-        if comanda = 2:
+        elif comanda == 2:
             comanda_completa.append("Pizza de glock")
-        if comanda = 3:
+        elif comanda == 3:
             comanda_completa.append("Limon")
-        if comanda = 4:
+        elif comanda == 4:
             comanda_completa.append("mas Pizza")
-        if comanda = 5:
+        elif comanda == 5:
             comanda_completa.append("Pizza de tierra")
-        if comanda = 6:
+        elif comanda == 6:
             break
+        else:
+            print("No valido")
+if opcion == 2:
+    print("Haciendo la masa...")
+    for i in range(100):
+        None
+    print("Añadiendo ingredientes al pan...")
+    for i in range(100):
+        None    
+    print("Cosechando limones...")
+    for i in range(100):
+        None
+    print("Encajando el pedido en caja...")
+    for i in range(100):
+        None
+    print("Listo para la entrega...")
+    for i in range(100):
+        None
+'''if opcion == 3:
+
+if opcion == 4:'''
